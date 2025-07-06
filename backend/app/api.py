1548a015e21b3f614a08e135627261e82fc7e9e7
Rishabh Malik <168853153+rishabh-malik-26@users.noreply.github.com>
2025-07-06 21:05:09 +0530

    Update api.py

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,5 +1,5 @@
 from fastapi  import FastAPI,Body
-from gemini_api import get_agent
+from .gemini_api import get_agent
 from pydantic import BaseModel
 import logging
 logging.basicConfig(level=logging.INFO,format= '%(asctime)s - %(levelname)s - %(message)s')
